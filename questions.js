ef132ecbcb88d8a7939f1ce25d9265593ec630a4
François Parlant <fxpar33@gmail.com>
2024-11-05 10:15:53 +0100

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,20 @@
+const questions = [
+	[ '01-Ai_hanmi_katatedori_ikkyo_omote_waza.mp4', 'Ai hanmi katate dori <u>ikkyo omote</u> waza'],
+	[ '02-Ai_hanmi_katatedori_ikkyo_ura_waza.mp4', 'Ai hanmi katate dori <u>ikkyo ura</u> waza'],
+	[ '03-Ai_hanmi_katatedori_nikyo_omote_waza.mp4', 'Ai hanmi katate dori <u>nikyo omote</u> waza'],
+	[ '04-Ai_hanmi_katatedori_nikyo_ura_waza.mp4', 'Ai hanmi katate dori <u>nikyo ura</u> waza'],
+	[ '05-Ai_Hamni_Katate_Dori_Sankyo_omoto_waza.mp4', 'Ai hanmi katate dori <u>sankyo omote</u> waza'],
+	[ '06-Ai_Hamni_Katate_Dori_sankyo_ura_waza.mp4', 'Ai hanmi katate dori <u>sankyo ura</u> waza'],
+	[ '07-Ai_Hamni_Katate_Dori_Yonkyo_Omote_waza.mp4', 'Ai hanmi katate dori <u>yonkyo omote</u> waza'],
+	[ '08-Ai_Hamni_Katate_Dori_yonkyo_ura_waza.mp4', 'Ai hanmi katate dori <u>yonkyo ura</u> waza'],
+	[ '09-Ai_Hamni_Katate_Dori_Shiho_nage_omote.mp4', 'Ai hanmi katate dori <u>Shiho nage omote</u> waza'],
+	[ '10-Ai_Hamni_Katate_Dori_Shiho_Nage_ura.mp4', 'Ai hanmi katate dori <u>Shiho nage ura</u> waza'],
+	[ '11-Ai_Hamni_Katate_Dori_Irimi_Nage_v1.mp4', 'Ai hanmi katate dori <u>Irimi nage v1</u> waza'],
+	[ '12-Ai_Hamni_Katate_Dori_Irimi_Nage_v2.mp4', 'Ai hanmi katate dori <u>Irimi nage v2</u> waza'],
+	[ '13-Ai_Hamni_Katate_Dori_Irimi_Nage_v3.mp4', 'Ai hanmi katate dori <u>Irimi nage v3</u> waza'],
+	[ '14-Ai_Hamni_Katate_Dori_KoteGaeshi_v1.mp4', 'Ai hanmi katate dori <u>Kote Gaeshi v1</u> waza'],
+	[ '15-Ai_Hamni_Katate_Dori_KoteGaeshi_v2.mp4', 'Ai hanmi katate dori <u>Kote Gaeshi v2</u> waza'],
+	[ '17-Ai_Hamni_Katate_Dori_Koshi_Nage.mp4', 'Ai hanmi katate dori <u>Irimi nage v2</u> waza'],
+	[ '18-Ai_Hamni_Katate_Dori_-_Ushiro_nage.mp4', 'Ai hanmi katate dori <u>Ushiro nage</u> waza'],
+	[ '19-Ai_Hamni_Katate_Dori_Ude_gatan.mp4', 'Ai hanmi katate dori <u>Ude Gatan</u> waza']
+]
---
+++ quiz.js
@@ -0,0 +1,26 @@
+
+console.log(questions.length);
+var arr = [];
+while(arr.length < 5){
+    var r = Math.floor(Math.random() * questions.length) + 1;
+    if(arr.indexOf(r) === -1) arr.push(r);
+}
+console.log(arr);
+
+var sct = "";
+for (let i = 0; i < arr.length; i++) {
+	console.log(i);
+	console.log(questions[arr[i]-1][1]);
+	sct += `<li class="slide">
+		<video src="assets/`+questions[arr[i]-1][0]+`" class="myvideo" alt="..." controls ></video>
+		<p>&nbsp;</p>
+		  <details><summary>Quelle est cette technique ?</summary>
+			  <h1>`+questions[arr[i]-1][1]+`</h1>
+		  </details>
+	</li>`;
+}
+
+var sc = document.getElementById("slides-container");
+sc.innerHTML = sct;
+
+  
